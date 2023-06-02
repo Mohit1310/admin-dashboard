fbc6c0714efd1520268180b1e2deeefb6f650a2e
Mohit Dayma <70746260+Mohit1310@users.noreply.github.com>
2023-06-02 13:54:14 +0530

    Minor Change

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Home.css'
 import FeaturedInfo from '../../components/FeaturedInfo/FeaturedInfo'
-import Chart from '../../components/charts/Chart'
+import Chart from '../../components/chart/Chart'
 import { userdata } from '../../dummytData'
 import WidgetsSm from '../../components/WidgetsSm/WidgetsSm'
 import WidgetsLg from '../../components/WidgetsLg/WidgetsLg'
@@ -10,7 +10,7 @@ export default function home() {
   return (
     <div className="home">
         <FeaturedInfo />
-        <Chart data={userdata} title="User Analytics" grid dataKey="name"/>
+        <Chart data={userdata} title="User Analytics" grid dataKey="Active User"/>
         <div className="homeWidgets">
           <WidgetsSm />
           <WidgetsLg />
